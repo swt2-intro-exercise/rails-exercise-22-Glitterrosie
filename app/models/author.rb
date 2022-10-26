1bd890df80e7268218f50bb6533c3c35620a9ec0
Glitterrosie <rosmarie.debski@gmail.com>
2022-10-26 17:38:26 +0200

    authors with an empty string as a last name will not be accepted

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,7 @@
 class Author < ApplicationRecord
-    def name()
+    validates :last_name, presence: true, length: { minimum: 1 }
+
+    def name
         return first_name + " " + last_name
     end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+
+    
+
     it "should exist with a first and last name and a homepage" do
+
         author = Author.new()
         author.first_name = 'Alan'
         author.last_name = 'Turing'
@@ -12,4 +16,11 @@ RSpec.describe Author, type: :model do
         expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
         expect(author.name()).to eq('Alan Turing')
     end
+
+    
+
+    it "shouldn't have an empty string as a last name" do
+        nonValidAuthor = Author.new()
+        expect(nonValidAuthor).to_not be_valid
+      end
 end
