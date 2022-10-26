0753f70bbc8b7a2e9b5ef8a3de3aa34dbbddcf44
Glitterrosie <rosmarie.debski@gmail.com>
2022-10-26 15:15:58 +0200

    you can now submit the form and the data will be stored in the database

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 describe "New author page", type: :feature do
+
   it "should exist at 'new_author_path' and render withour error" do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
   end
+
   it "should have text inputs for an author's first name, last name, and homepage" do
     visit new_author_path
     # these are the standard names given to inputs by the Rails form builder
@@ -12,4 +14,15 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "should be an entry in the authors db after submitting a author form" do
+    visit new_author_path
+    # fill the input fields
+    page.fill_in 'author[first_name]', with: 'Ada'
+    page.fill_in 'author[last_name]', with: 'Lovelace'
+    page.fill_in 'author[homepage]', with: 'https://de.wikipedia.org/wiki/Ada_Lovelace'
+    # submitting the form
+    page.find('input[type="submit"]').click
+  end
+
 end
