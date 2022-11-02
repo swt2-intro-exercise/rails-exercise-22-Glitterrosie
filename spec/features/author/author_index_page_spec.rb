a4cdeb40bca06b3c84ccc75ece93d90c50a4814a
Glitterrosie <rosmarie.debski@gmail.com>
2022-11-02 18:14:10 +0100

    added feature to delete authors

--- spec/features/author/author_index_page_spec.rb
+++ spec/features/author/author_index_page_spec.rb
@@ -17,4 +17,19 @@ describe "author index page", type: :feature do
         expect(page).to have_link 'New', href: new_author_path
     end
 
+    it "should have a link to delete authors" do
+        # save a new author
+        author = FactoryBot.create :author
+
+        visit new_author_path
+        page.fill_in 'author[first_name]', with: author.first_name
+        page.fill_in 'author[last_name]', with: author.last_name
+        page.fill_in 'author[homepage]', with: author.homepage
+        # submitting the form
+        page.find('input[type="submit"]').click
+
+        visit authors_url
+        expect(page).to have_link 'Delete', href: author_path(author)
+    end
+
 end
