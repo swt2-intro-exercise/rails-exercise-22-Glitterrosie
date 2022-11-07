f1d7e2894ae085e75206fa45ae814440b29fceed
Glitterrosie <rosmarie.debski@gmail.com>
2022-11-07 20:16:00 +0100

    resolves #17

--- spec/features/paper/edit_paper_page_spec.rb
+++ spec/features/paper/edit_paper_page_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 
 describe "Edit paper page", type: :feature do
 
-    #paper = Paper.new(title: "Bla", venue: "blub", year: 200)
     paper = FactoryBot.create :paper
 
   it "should exist at 'edit_paper_path' and render withour error" do
     visit edit_paper_path(paper)
   end
 
-  it "should have text inputs for an author's first name, last name, and homepage with the right author information" do
+  it "should have text inputs for an author's first name, last name, and homepage with the right author information and a multiselect box for authors" do
     visit edit_paper_path(paper)
 
     page.find_field('paper[title]', with: paper.title)
-    page.find_field('paper[venue]', with:paper.venue)
-    page.find_field('paper[year]', with:paper.year)
+    page.find_field('paper[venue]', with: paper.venue)
+    page.find_field('paper[year]', with: paper.year)
+    #expect(page). to have_select('author_ids')
+    #page.find_field('paper[authors]')
+    
   end
 
   it "after submitting the edited paper should be saved" do
